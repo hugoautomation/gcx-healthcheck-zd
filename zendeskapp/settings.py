c8e603827b13578b7be4dd841bf578739b70389d
hugoautomation <zhugok@gmail.com>
2024-10-26 11:05:46 +1100

    updated mime type

--- zendeskapp/settings.py
+++ zendeskapp/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import mimetypes
+mimetypes.add_type("text/javascript", ".js", True)
 mimetypes.add_type("text/css", ".css", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DJANGO_ENV = os.environ.get("RAILWAY_ENVIRONMENT_NAME", "development")
