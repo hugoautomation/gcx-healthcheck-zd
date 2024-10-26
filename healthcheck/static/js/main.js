6aced73975c5751506fda117eb63b0e624ade06c
hugoautomation <zhugok@gmail.com>
2024-10-26 13:17:20 +1100

    fixed the loading

--- healthcheck/static/js/main.js
+++ healthcheck/static/js/main.js
@@ -18,7 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
         resultsDiv.innerHTML = `
             <div class="text-center my-5">
                 <div class="spinner-border text-primary" role="status">
-                    <span class="visually-hidden">Loading...</span>
                 </div>
             </div>
         `;
