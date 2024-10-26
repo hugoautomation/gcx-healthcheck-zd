de4dd73fdf72d8b064644a5fe19dfe6b6c3b5a8d
hugoautomation <zhugok@gmail.com>
2024-10-26 11:03:34 +1100

    fixing mime

--- zendeskapp/settings.py
+++ zendeskapp/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DJANGO_ENV = os.environ.get("RAILWAY_ENVIRONMENT_NAME", "development")
