9c6b3e2e545df1fac790a119279f6b01c9ecc2e4
hugoautomation <zhugok@gmail.com>
2024-12-20 08:52:19 +1100

    updated stripe version

--- healthcheck/views.py
+++ healthcheck/views.py
@@ -859,6 +859,7 @@ def create_checkout_session(request):
             return JsonResponse({"error": "User not found"}, status=404)
 
         # Create Stripe checkout session
+        stripe.api_version = "2020-03-02"
         stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=['card'],
