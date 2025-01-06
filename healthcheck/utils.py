701a21150061050bd0ba310e04aa0d78c4fe5996
hugoautomation <zhugok@gmail.com>
2025-01-07 10:16:08 +1100

    updated webhook

--- healthcheck/utils.py
+++ healthcheck/utils.py
@@ -1,7 +1,25 @@
 from django.template.loader import render_to_string
 from .models import HealthCheckMonitoring
 from django.utils.timesince import timesince
+from zendeskapp import settings
+from djstripe.models import WebhookEndpoint
+from django.urls import reverse
 
+def create_webhook_endpoint(request):
+    """Create or get a webhook endpoint"""
+    webhook_url = request.build_absolute_uri('/stripe/webhook/')
+    
+    # Try to get existing webhook or create new one
+    webhook_endpoint = WebhookEndpoint.objects.filter(url=webhook_url).first()
+    if not webhook_endpoint:
+        webhook_endpoint = WebhookEndpoint.objects.create(
+            url=webhook_url,
+            secret=settings.DJSTRIPE_WEBHOOK_SECRET,
+            active=True,
+            # api_version=settings.STRIPE_API_VERSION,
+        )
+    
+    return webhook_endpoint
 
 def format_response_data(
     response_data,
