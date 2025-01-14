5c2a8ffb786cd8c40bce4cb184efae6b6f91076d
hugoautomation <zhugok@gmail.com>
2025-01-14 09:59:32 -0800

    updates start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# Start Celery workers
-celery -A zendeskapp worker --loglevel=info --concurrency=8 &
-
-# Start Django
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn zendeskapp.wsgi:application --timeout 120 --workers 2 --threads 2 --worker-class gthread --bind 0.0.0.0:8000
+celery -A zendeskapp worker --loglevel=info --concurrency=8 & python manage.py migrate && python manage.py collectstatic --noinput && gunicorn zendeskapp.wsgi:application --timeout 120 --workers 2 --threads 2 --worker-class gthread --bind 0.0.0.0:8000 --log-level debug --error-logfile - --access-logfile - --capture-output --enable-stdio-inheritance
