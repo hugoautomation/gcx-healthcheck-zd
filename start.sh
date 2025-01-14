e3445b5ebce7cf8a76ae8cde06618de3c557ebe4
hugoautomation <zhugok@gmail.com>
2025-01-14 10:09:26 -0800

    updated start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ celery -A zendeskapp worker --loglevel=info --concurrency=8 &
 # Start Django
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn zendeskapp.wsgi:application --timeout 120 --workers 2 --threads 2 --worker-class gthread
+gunicorn zendeskapp.wsgi:application --timeout 120 --workers 2 --threads 2 --keep-alive 120 --max-requests-jitter 50 --max-requests 1000 --worker-class --graceful-timeout 120 gthread
